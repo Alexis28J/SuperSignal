a165e63386327561390f83d12efcee4f4ce94e9a
Jordy Alexis <jordy2806@hotmail.com>
2026-02-25 13:28:21 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 //per usare una libreria (fatta da me o di terze parti) devo importarla
 //importiamo le funzioni signal ed effect dal file signal.js
-import { signal, effect } from './signal.js';
+//import { signal, effect, computed } from './signal.js';
 
-const pippo = signal('Pippo ha caldo');
-//signal è una funzione che restituisce un oggetto con una proprietà value che contiene il valore passato come argomento
+// const pippo = signal('Pippo ha caldo');
+// //signal è una funzione che restituisce un oggetto con una proprietà value che contiene il valore passato come argomento
 
-console.log('Valore dentro il signal:', pippo());
+// const pippoLenght = computed(() => pippo().length, [pippo]);
 
-pippo.set('Leonardo sente la mancanza di Copilot');
+// //console.log('Valore dentro il signal:', pippo());
+// effect(() => console.log("E' partito effect", pippo(), pippoLenght()), [pippo]);
 
-console.log('Valore dentro il signal:', pippo());
+// pippo.set('Leonardo sente la mancanza di Copilot');
 
-pippo.update((value) => value + ' e ha fame');
-//Sto passando una funzione lambda all'update
+// console.log('Valore dentro il signal:', pippo());
 
-console.log('Valore dentro il signal:', pippo());
+// pippo.update((value) => value + ' e ha fame');
+// //Sto passando una funzione lambda all'update
+
+// console.log('Valore dentro il signal:', pippo());
 
 
-effect(() => console.log('Valore dentro il signal', pippo()));
+import { MeteoService } from "./meteo-service.js";
+import { effect } from "./signal.js";
 
+const mServ = new MeteoService;
 
+//mServ.getTemperature();
+
+effect(() => console.log("Sono l'effect. Le temperature sono cambiate", mServ.tempSignal(), mServ.celsiusSignal()), [mServ.tempSignal]);
+
+effect(() => {
+    for (let i = 0; i < mServ.tempSignal().length; i++) {
+        const card = `<span>time: ${mServ.tempSignal()[i].time}, Fahrenheit: ${mServ.tempSignal()[i].temp}, Celsius: ${mServ.celsiusSignal()[i].temp}</span>`
+        document.body.innerHTML += card;
+    }
+}, [mServ.tempSignal, mServ.celsiusSignal]);
--- signal.js
+++ signal.js
@@ -9,19 +9,76 @@ export function signal(value) {
     const signalFunction = () => signalValue;
     //se viene chiamata (), senza parametri, restituisce il valore attuale del signal
 
-    signalFunction.set = (newValue) => signalValue = newValue;  
+
+    const notify = () => {
+       for (const followerFunction of followers) {
+         followerFunction();
+       }
+    }
+    //Chiamiamo notify ogni volta che il valore del signal cambia, in modo da notificare tutti i follower che il valore è cambiato
+
+
+    //signalFunction.set = (newValue) => signalValue = newValue;  
     //una funzione può avere proprietà, in questo caso set è una proprietà che è una funzione che prende un nuovo valore e lo assegna a signalValue
     //se viene chiamata set con un nuovo valore, aggiorna il valore del signal
+    
+    signalFunction.set = (newValue) => { 
+        signalValue = newValue;
+        notify()
+    }
 
-    signalFunction.update = (updateFunction) => signalValue = updateFunction(signalValue);
+    //signalFunction.update = (updateFunction) => signalValue = updateFunction(signalValue);
     //update è una proprietà che è una funzione che prende una funzione di aggiornamento, che a sua volta prende il valore attuale del signal e restituisce un nuovo valore
     //se viene chiamata update con una funzione di aggiornamento, aggiorna il valore del signal con il risultato della funzione di aggiornamento
     //la differenza tra set e update è che set prende un nuovo valore direttamente, mentre update prende una funzione che calcola il nuovo valore a partire da quello attuale
+
+    signalFunction.update = (updateFunction) => {
+        signalValue = updateFunction(signalValue);
+        notify();
+    }
+
+    signalFunction.addFollower = (followerFunction) => followers.push(followerFunction);
+    
 
     return signalFunction;
     
 }
 
-export function effect(effectFunction) {
+//efectFunction => () => console.log('Valore dentro il signal', pippo())
+//signals => [pippo]
+export function effect(effectFunction, signals = []) {
+
+    for (const signal of signals) {
+        signal.addFollower(effectFunction);
+        effectFunction();
+    }
+}
+
+//computedFunction => () => pippo().length
+//signals => [pippo]
+export function computed(computedFunction, signals = []) {
     
+    let computedValue = computedFunction();
+
+    const followers = [];
+
+    const notify = () => {
+        
+        for (const followerFunction of followers) {
+            followerFunction();
+        }
+    }
+
+    const signalFunction = () => computedValue;
+
+    signalFunction.addFollower = (follower) => followers.push(follower);
+
+    for (const signal of signals) {
+        signal.addFollower(() => {
+            computedValue = computedFunction();
+            notify();
+        })
+    }
+    
+    return signalFunction;
 }
